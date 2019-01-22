c3edc2afccd8b518d3f908e77ec5eb43edaead2d
Jake Champion <jakechampion.jake2@googlemail.com>
2019-01-22 16:53:24 +0000

    simplify

--- fastly/terraform/main.tf
+++ fastly/terraform/main.tf
@@ -2,27 +2,13 @@ provider "fastly" {
   version = "0.1.2"
 }
 
-variable "domain" {
-  type    = "string"
-  default = "test.in.ft.com"
-}
-
-variable "name" {
-  type    = "string"
-  default = "Origami Polyfill Service"
-}
-
-output "service_id" {
-  value = ["${fastly_service_v1.app.id}"]
-}
-
 resource "fastly_service_v1" "app" {
   name = "polyfill-useragent-normaliser"
 
   force_destroy = false
 
   domain {
-    name = "${var.domain}"
+    name = "test.in.ft.com"
   }
 
   vcl {
